6e36b1aa25e530a308dcd8a4862f6629b16d7161
Kid A <thisismeamir@outlook.com>
2025-02-08 20:40:09 +0330

    [bug] Getting ready for maven publication

--- kortex-api/src/test/kotlin/InitializingTests.kt
+++ kortex-api/src/test/kotlin/InitializingTests.kt
@@ -1,14 +1,15 @@
+import io.github.thisismeamir.kortex.Kortex
 import org.junit.jupiter.api.Test
 
 class InitializingTests {
 
-    // Should not be running, currently one must start
-    // cortex.cpp from terminal by themselves.
+//     Should not be running, currently one must start
+//     cortex.cpp from terminal by themselves.
 //    @Test
 //    fun `Cortex starts`() {
 //        val kortex = Kortex()
 //    }
-
+//
 //    @Test
 //    fun `Cortex stops`(){
 //        val kortex = Kortex()
