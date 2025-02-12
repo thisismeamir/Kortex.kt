a1de25150d4c0b08b48e5433e201c5db2cfc5a84
Kid A <thisismeamir@outlook.com>
2025-02-12 16:51:38 +0330

    [refactor] optimizing imports

--- kortex-api/src/main/kotlin/io/github/thisismeamir/kortex/objs/hardware/HardwareInformation.kt
+++ kortex-api/src/main/kotlin/io/github/thisismeamir/kortex/objs/hardware/HardwareInformation.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HardwareInformation(
-    val cpu: io.github.thisismeamir.kortex.objs.hardware.CpuInformation,
-    val gpus: List<io.github.thisismeamir.kortex.objs.hardware.GpuInformation>,
-    val os: io.github.thisismeamir.kortex.objs.hardware.OsInformation,
-    val power: io.github.thisismeamir.kortex.objs.hardware.PowerInformation,
-    val ram: io.github.thisismeamir.kortex.objs.hardware.RamInformation,
-    val storage: io.github.thisismeamir.kortex.objs.hardware.StorageInformation
+    val cpu: CpuInformation,
+    val gpus: List<GpuInformation>,
+    val os: OsInformation,
+    val power: PowerInformation,
+    val ram: RamInformation,
+    val storage: StorageInformation
 )
