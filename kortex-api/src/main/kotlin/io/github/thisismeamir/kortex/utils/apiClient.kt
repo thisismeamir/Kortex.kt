f1783ce5ef6bc0cf83762273d45736e10b68a542
Kid A <thisismeamir@outlook.com>
2025-03-16 19:35:35 +0330

    [refactor] change of packaging

--- kortex-api/src/main/kotlin/io/github/thisismeamir/kortex/utils/apiClient.kt
+++
@@ -1,8 +0,0 @@
-package io.github.thisismeamir.kortex.utils
-
-import io.github.thisismeamir.kortex.Kortex
-import io.github.thisismeamir.kortex.client.infrastructure.ApiClient
-import io.ktor.client.engine.cio.CIO
-
-val apiClient = ApiClient(baseUrl = "http://127.0.0.1:39281", httpClientEngine = CIO.create())
-val myService = apiClient.createService(Kortex::class)
